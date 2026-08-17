a4321bc187f613416559ee6aa1af138abaf49f6d
DAI-CHI WU <dakinwu20@gmail.com>
2026-08-17 15:29:52 +0800

    Add files via upload

--- cloud-config.js
+++ cloud-config.js
@@ -2,5 +2,5 @@ window.TRIP_CLOUD_CONFIG = {
   // 只需把 YOUR_PROJECT_REF 換成 Supabase Project Reference。
   // 例：https://abcdefghijklmnopqrst.supabase.co/functions/v1/trip-state
   functionUrl: 'https://zahmltswbiucslmboxwt.supabase.co/functions/v1/trip-state',
-  syncIntervalMs: 15000
+  syncIntervalMs: 60000
 };
