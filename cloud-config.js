ba5afba6d1a6646bd10cd2b01b41357213c7a324
DAI-CHI WU <dakinwu20@gmail.com>
2026-08-18 14:31:09 +0800

    Add files via upload

--- cloud-config.js
+++ cloud-config.js
@@ -1,6 +1,6 @@
 window.TRIP_CLOUD_CONFIG = {
   namespace: 'nagoya-hokuriku-v6-2027',
   // 把 YOUR_PROJECT_REF 換成你的 Supabase Project Reference。
-  functionUrl: 'https://YOUR_PROJECT_REF.supabase.co/functions/v1/trip-state',
+  functionUrl: 'https://zahmltswbiucslmboxwt.supabase.co/functions/v1/trip-state',
   syncIntervalMs: 60000
 };
